2cd1c0e475d4539985904c937b1dbf10ab4d3e42
TianGuanghui <tgh949909209@126.com>
2017-08-01 19:03:09 +0800

    format

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ import 'fastclick';
 import 'isomorphic-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import '../styles/main.scss';
 import App from './containers/App';
 import configureStore from './store/configureStore';
@@ -11,8 +11,8 @@ import configureStore from './store/configureStore';
 const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('main')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('main')
 );
